b00e6c3e27ba95801377ed5ea25a96f3528a5a9e
Alexander Sotskii <64553081+Alexanderwebsot@users.noreply.github.com>
2021-06-02 19:37:11 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -7,6 +7,54 @@ $(document).ready(function () {
 	  prevArrow: $('.projects-arrow__l'),
 	  nextArrow: $('.projects-arrow__r')
   });
+  $('.team-slider').slick({
+      infinite: false,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      dots: false,
+      prevArrow: $('.projects-arrow__l'),
+      nextArrow: $('.projects-arrow__r'),
+      responsive: [
+          {
+            breakpoint: 992,
+            settings: {
+              dots: true,
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+    });
+  $('.projects-slider-top').slick({
+  	  infinite: false,
+  	  slidesToShow: 1,
+  	  slidesToScroll: 1,
+  	  dots: false,
+  	  arrows: false,
+    });
+  $('.projects-slid_1').on('click', function() {
+  	$('.projects-slider-top').slick('slickGoTo', 0);
+  	return false;
+  })
+  $('.projects-slid_2').on('click', function() {
+  	$('.projects-slider-top').slick('slickGoTo', 1);
+  	return false;
+  })
+  $('.projects-slid_3').on('click', function() {
+  	$('.projects-slider-top').slick('slickGoTo', 2);
+  	return false;
+  })
+  $('#ipt-file').on('change', function() {
+  	let file = $('#ipt-file')[0].files[0]
+  	if (file){
+  	  $('.ipt-label-text')[0].innerHTML = file.name;
+  	}
+  })
+  $('#select-request').selectize({
+    create: false,
+    sortField: 'text'
+	});
   new WOW().init();
   function slickify(){
     $('.documents-row').slick({
